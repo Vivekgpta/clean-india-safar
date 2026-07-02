10b3022cc26d8d27d4f446f0f05d762abac867f4
Vivekgpta <131643827+Vivekgpta@users.noreply.github.com>
2026-07-02 10:53:21 +0530

    Gallery updated

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,34 +4,6 @@ import "./Gallery.css";
 import Navbar from "../Navbar/Navbar";
 import VideoGallery from "../VideoGallery/VideoGallery";
 import ReelsGallery from "../ReelsGallery/ReelsGallery";
-// export default function Gallery() {
-//   const gallery = [
-//     //  "/src/assets/aboutUs/aboutUs1.png",
-//     // "",
-//     "/src/assets/gallery/img2.jpg",
-//     // "/src/assets/gallery/img3.jpg",
-//     "/src/assets/gallery/img4.jpg",
-//     "/src/assets/gallery/img5.jpg",
-//     "/src/assets/gallery/img6.jpg",
-//     "/src/assets/gallery/img7.jpg",
-//     "/src/assets/gallery/img9.jpg",
-//     // "/src/assets/gallery/img8.jpg",
-//     // "/src/assets/gallery/img2.jpg",
-//     // "/src/assets/aboutUs/aboutUs1.png",
-//     // "/src/assets/aboutUs/aboutUs1.png",
-//     // "/src/assets/aboutUs/aboutUs1.png",
-//     // "/src/assets/aboutUs/aboutUs1.png",
-//     // "/gallery/gallery3.jpg",
-//     // "/gallery/gallery4.jpg",
-//     // "/gallery/gallery5.jpg",
-//     // "/gallery/gallery6.jpg",
-//     // "/gallery/gallery7.jpg",
-//     // "/gallery/gallery8.jpg",
-//     // "/gallery/gallery9.jpg",
-//     // "/gallery/gallery10.jpg",
-//     // "/gallery/gallery11.jpg",
-//     // "/gallery/gallery12.jpg",
-//   ];
 
 import img2 from "../../assets/gallery/img2.jpg";
 import img4 from "../../assets/gallery/img4.jpg";
@@ -40,14 +12,35 @@ import img6 from "../../assets/gallery/img6.jpg";
 import img7 from "../../assets/gallery/img7.jpg";
 import img9 from "../../assets/gallery/img9.jpg";
 
-const gallery = [
-  img2,
-  img4,
-  img5,
-  img6,
-  img7,
-  img9,
-];
+export default function Gallery() {
+  // const gallery = [
+  //   //  "/src/assets/aboutUs/aboutUs1.png",
+  //   // "",
+  //   "/src/assets/gallery/img2.jpg",
+  //   // "/src/assets/gallery/img3.jpg",
+  //   "/src/assets/gallery/img4.jpg",
+  //   "/src/assets/gallery/img5.jpg",
+  //   "/src/assets/gallery/img6.jpg",
+  //   "/src/assets/gallery/img7.jpg",
+  //   "/src/assets/gallery/img9.jpg",
+  //   // "/src/assets/gallery/img8.jpg",
+  //   // "/src/assets/gallery/img2.jpg",
+  //   // "/src/assets/aboutUs/aboutUs1.png",
+  //   // "/src/assets/aboutUs/aboutUs1.png",
+  //   // "/src/assets/aboutUs/aboutUs1.png",
+  //   // "/src/assets/aboutUs/aboutUs1.png",
+  //   // "/gallery/gallery3.jpg",
+  //   // "/gallery/gallery4.jpg",
+  //   // "/gallery/gallery5.jpg",
+  //   // "/gallery/gallery6.jpg",
+  //   // "/gallery/gallery7.jpg",
+  //   // "/gallery/gallery8.jpg",
+  //   // "/gallery/gallery9.jpg",
+  //   // "/gallery/gallery10.jpg",
+  //   // "/gallery/gallery11.jpg",
+  //   // "/gallery/gallery12.jpg",
+  // ];
+  const gallery = [img2, img4, img5, img6, img7, img9];
 
   return (
     // <MainLayout>
@@ -71,7 +64,7 @@ const gallery = [
           </div>
         ))}
       </div>
-      
+
       {/* <VideoGallery/> */}
     </section>
     // </MainLayout>
